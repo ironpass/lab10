ddfd6d95d9660b35ebc141f4935152b3c229684d
Ood <akarachai45525@hotmail.com>
2016-04-30 18:37:15 +0700

    start to create UI

---
+++ src/UI/CoinCounterUI.java
@@ -0,0 +1,15 @@
+package UI;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class CoinCounterUI extends JFrame {
+	public CoinCounterUI(){
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		initComponents();
+		setVisible(true);
+	}
+	public void initComponents() {
+		
+	}
+}
---
+++ src/UI/CoinMachineUI.java
@@ -0,0 +1,16 @@
+package UI;
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+public class CoinMachineUI extends JFrame {
+	public CoinMachineUI() {
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		initComponents();
+		setVisible(true);
+	}
+	
+	public void initComponents() {
+		
+	}
+}
